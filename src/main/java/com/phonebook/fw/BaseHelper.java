d303e7403f3b4347a3d1e501c3087846758403dc
Edgar-Arushanyan <edgar1984.de@gmail.com>
2023-06-29 10:53:39 +0200

    'update'

--- src/main/java/com/phonebook/fw/BaseHelper.java
+++ src/main/java/com/phonebook/fw/BaseHelper.java
@@ -1,12 +1,12 @@
 package com.phonebook.fw;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class BaseHelper {
@@ -58,5 +58,21 @@ public class BaseHelper {
             alert.accept();//robot podtwerschdaet knopku ok na wspliwajuschem okne
             return true;
         }
+    }
+
+    public String takeScreenshot() {
+       File tmp = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+      File screenshot = new File("screenshots/screen" + System.currentTimeMillis() + ".png");
+
+        try {
+            Files.copy(tmp, screenshot);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return screenshot.getAbsolutePath();
+
+
     }
 }
